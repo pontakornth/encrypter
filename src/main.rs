5603c8e82d97dc228c03b8f9d570f8a2e5aced46
pontakornth <pontakorn61@gmail.com>
2020-05-08 10:13:48 +0700

    feat: Add input and output arg

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,36 @@ use aes_ctr::stream_cipher::generic_array::{GenericArray};
 use aes_ctr::stream_cipher::{
     NewStreamCipher, SyncStreamCipher, SyncStreamCipherSeek
 };
+use clap::{Arg, App};
 use sha2::{Sha256, Digest};
 fn main() {
+    let matches = App::new("Encrypter")
+                     .version("0.1")
+                     .author("Pontakorn Paesaeng <pontakorn61@gmail.com>")
+                     .about("Encrypt and decrypt file")
+                     .arg(Arg::with_name("mode")
+                       .short("m")
+                       .long("mode")
+                       .value_name("MODE")
+                       .help("Set mode")
+                       .takes_value(true)
+                    )
+                      .arg(Arg::with_name("INPUT")
+                        .help("Set input file")
+                        .required(true)
+                        .index(1)
+                    )
+                      .arg(Arg::with_name("OUTPUT")
+                        .help("Set output file")
+                        .required(true)
+                        .index(2)
+                    )
+                    .get_matches();
+
     let key = Sha256::digest(b"J");
     let nouce = GenericArray::from_slice(b"F6477DDFF97D263D");
     let mut cipher = Aes256Ctr::new(&key, &nouce);
-    let file = File::open("text.txt").unwrap();
+    let file = File::open(matches.value_of("INPUT").unwrap()).unwrap();
     let mut buf_reader = BufReader::new(file);
     let mut contents = vec![];
     match buf_reader.read_to_end(&mut contents) {
@@ -23,7 +47,7 @@ fn main() {
     }
     let mut block = contents;
     cipher.apply_keystream(&mut block);
-    let output_file = File::create("./out/text_out.txt").unwrap();
+    let output_file = File::create(matches.value_of("OUTPUT").unwrap()).unwrap();
     let mut writer = BufWriter::new(output_file);
     writer.write(&block).unwrap();
     let output_file_again = File::open("./out/text_out.txt").unwrap();
